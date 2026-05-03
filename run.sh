9b25a455da978f632744e49ccae41af829baf145
SharkPlush <86890204+SharkPlush@users.noreply.github.com>
2026-05-03 16:22:05 +0300

    .

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export APPIMAGE="/app/bin/ledger-live-desktop"
-exec zypak-wrapper /app/bin/ledger-live-desktop --ozone-platform=wayland --enable-features=UseOzonePlatform --disable-gpu-memory-buffer-video-frames "$@"
